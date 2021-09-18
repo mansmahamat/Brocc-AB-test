9209fe95cec1abbb4f8921429d7df6f96149245a
mansmahamat <mansour.mahamat.salle@gmail.com>
2021-09-18 20:53:50 +0200

    :art: delete github info page
    delete github info page

--- src/api/GithubApi.js
+++ src/api/GithubApi.js
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 const baseUrl = 'https://api.github.com/repos';
 
-export const getFacebook = () =>
-  axios.get(`${baseUrl}/eslint/eslint`).then((res) => res.data);
-
 export const getRepository = (name) =>
   axios.get(`${baseUrl}/${name}`).then((res) => res.data);
